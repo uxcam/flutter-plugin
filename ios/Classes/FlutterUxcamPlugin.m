1ffc836fad91c9e6683cc404cccd1c9533cfc21d
samin20 <samin@uxcam.com>
2021-02-25 18:05:27 +0545

    all void APIs returned in iOS

--- ios/Classes/FlutterUxcamPlugin.m
+++ ios/Classes/FlutterUxcamPlugin.m
@@ -28,38 +28,48 @@
 	else if ([@"startNewSession" isEqualToString:call.method])
 	{
         [UXCam startNewSession];
+        result(nil);
     }
 	else if ([@"stopSessionAndUploadData" isEqualToString:call.method])
 	{
         [UXCam stopSessionAndUploadData];
+        result(nil);
     }
 	else if ([@"allowShortBreakForAnotherApp" isEqualToString:call.method])
 	{
         BOOL continueSession = call.arguments[@"key"];
         [UXCam allowShortBreakForAnotherApp:continueSession];
+        result(nil);
     } else if ([@"occludeSensitiveScreen" isEqualToString:call.method]) {
         NSNumber *value = call.arguments[@"key"];
         [UXCam occludeSensitiveScreen:[value boolValue]];
+        result(nil);
     } else if ([@"occludeSensitiveScreenWithoutGesture" isEqualToString:call.method]) {
         NSNumber *value = call.arguments[@"key"];
         NSNumber *withoutGesture = call.arguments[@"withoutGesture"];
         [UXCam occludeSensitiveScreen:[value boolValue] hideGestures:[withoutGesture boolValue]];
+        result(nil);
     } else if ([@"occludeAllTextFields" isEqualToString:call.method]) {
         NSNumber *occludeAllTextField = call.arguments[@"key"];
         [UXCam occludeAllTextFields:[occludeAllTextField boolValue]];
+        result(nil);
     } else if ([@"occludeAllTextView" isEqualToString:call.method]) {
         NSLog(@"UXCam: Use occludeAllTextFields instead.");
+        result(nil);
     }
 	else if ([@"tagScreenName" isEqualToString:call.method])
 	{
         NSString* eventName = call.arguments[@"key"];
         [UXCam tagScreenName:eventName];
+        result(nil);
     } else if ([@"setAutomaticScreenNameTagging" isEqualToString:call.method]) {
         NSNumber *enable = call.arguments[@"key"];
         [UXCam setAutomaticScreenNameTagging:[enable boolValue]];
+        result(nil);
     } else if ([@"setMultiSessionRecord" isEqualToString:call.method]) {
         NSNumber *enable = call.arguments[@"key"];
         [UXCam setMultiSessionRecord:[enable boolValue]];
+        result(nil);
     } else if ([@"getMultiSessionRecord" isEqualToString:call.method]) {
         BOOL status =  [UXCam getMultiSessionRecord];
         result(@(status));
@@ -68,12 +78,14 @@
 	{
         NSString* userIdentity = call.arguments[@"key"];
         [UXCam setUserIdentity:userIdentity];
+        result(nil);
     }
 	else if ([@"setUserProperty" isEqualToString:call.method])
 	{
         NSString* userProperty = call.arguments[@"key"];
         NSString* value = call.arguments[@"value"];
         [UXCam setUserProperty:userProperty value:value];
+        result(nil);
     }
 	else if ([@"logEvent" isEqualToString:call.method])
 	{
@@ -82,6 +94,7 @@
 		{
             [UXCam logEvent:eventName];
         }
+        result(nil);
     }
 	else if ([@"logEventWithProperties" isEqualToString:call.method])
 	{
@@ -91,6 +104,7 @@
 		{
             [UXCam logEvent:tag withProperties:properties];
         }
+        result(nil);
     }
 	else if ([@"isRecording" isEqualToString:call.method])
 	{
@@ -100,18 +114,22 @@
 	else if ([@"pauseScreenRecording" isEqualToString:call.method])
 	{
         [UXCam pauseScreenRecording];
+        result(nil);
     }
 	else if ([@"resumeScreenRecording" isEqualToString:call.method])
 	{
         [UXCam resumeScreenRecording];
+        result(nil);
     }
 	else if ([@"optInOverall" isEqualToString:call.method])
 	{
         [UXCam optInOverall];
+        result(nil);
     }
 	else if ([@"optOutOverall" isEqualToString:call.method])
 	{
         [UXCam optOutOverall];
+        result(nil);
     }
 	else if ([@"optInOverallStatus"isEqualToString:call.method])
 	{
@@ -120,10 +138,12 @@
 	else if ([@"optIntoVideoRecording" isEqualToString:call.method])
 	{
         [UXCam optIntoSchematicRecordings];
+        result(nil);
     }
 	else if ([@"optOutOfVideoRecording" isEqualToString:call.method])
 	{
         [UXCam optOutOfSchematicRecordings];
+        result(nil);
     }
 	else if ([@"optInVideoRecordingStatus"isEqualToString:call.method])
 	{
@@ -132,10 +152,12 @@
 	else if ([@"optIntoSchematicRecordings" isEqualToString:call.method])
 	{
         [UXCam optIntoSchematicRecordings];
+        result(nil);
     }
 	else if ([@"optOutOfSchematicRecordings" isEqualToString:call.method])
 	{
         [UXCam optOutOfSchematicRecordings];
+        result(nil);
     }
 	else if ([@"optInSchematicRecordingStatus"isEqualToString:call.method])
 	{
@@ -144,10 +166,12 @@
 	else if ([@"cancelCurrentSession"isEqualToString:call.method])
 	{
         [UXCam cancelCurrentSession];
+        result(nil);
     }
 	else if ([@"deletePendingUploads"isEqualToString:call.method])
 	{
         [UXCam deletePendingUploads];
+        result(nil);
     }
 	else if ([@"pendingUploads"isEqualToString:call.method])
 	{
@@ -156,15 +180,18 @@
     else if ([@"uploadPendingSession"isEqualToString:call.method])
 	{
         [UXCam uploadingPendingSessions:nil];
+        result(nil);
     }
 	else if ([@"stopApplicationAndUploadData"isEqualToString:call.method])
 	{
         [UXCam stopSessionAndUploadData];
+        result(nil);
     }
 	else if ([@"tagScreenName"isEqualToString:call.method])
 	{
         NSString* screenName = call.arguments;
         [UXCam tagScreenName:screenName];
+        result(nil);
     }
 	else if ([@"urlForCurrentUser"isEqualToString:call.method])
 	{
@@ -179,25 +206,30 @@
 	else if ([@"addVerificationListener"isEqualToString:call.method])
 	{
         NSLog(@"UXCam: addVerificationListener is not supported by UXCam iOS.");
+        result(nil);
     }
 	else if ([@"addScreenNameToIgnore" isEqualToString:call.method])
 	{
         NSString* eventName = call.arguments[@"key"];
         [UXCam addScreenNameToIgnore:eventName];
+        result(nil);
     }
 	else if ([@"removeScreenNameToIgnore" isEqualToString:call.method])
 	{
         NSString* eventName = call.arguments[@"key"];
        [UXCam removeScreenNameToIgnore:eventName];
+       result(nil);
     }
 	else if ([@"removeAllScreenNamesToIgnore" isEqualToString:call.method])
 	{
        [UXCam removeAllScreenNamesToIgnore];
+       result(nil);
     }
     else if ([@"setPushNotificationToken" isEqualToString:call.method])
 	{
         NSString* token = call.arguments[@"key"];
         [UXCam setPushNotificationToken:token];
+        result(nil);
     }
     else if ([@"reportBugEvent" isEqualToString:call.method])
 	{
@@ -210,6 +242,7 @@
         {
             [UXCam reportBugEvent:eventName properties:nil];
         }
+        result(nil);
     }
 	else
 	{
