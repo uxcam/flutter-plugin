85eb36769b22ce53a04fe54c3f8072f980d0ce6a
Anup Khanal <anup@uxcam.com>
2022-12-07 17:22:00 +0545

    feat: Updated android sdk dependency to 3.5.2 and flutter sdk version to 2.2.2

--- android/src/main/java/com/uxcam/flutteruxcam/FlutterUxcamPlugin.java
+++ android/src/main/java/com/uxcam/flutteruxcam/FlutterUxcamPlugin.java
@@ -31,7 +31,7 @@ import androidx.annotation.NonNull;
  * FlutterUxcamPlugin
  */
 public class FlutterUxcamPlugin implements MethodCallHandler, FlutterPlugin, ActivityAware {
-    private static final String TYPE_VERSION = "2.2.1";
+    private static final String TYPE_VERSION = "2.2.2";
     public static final String TAG = "FlutterUXCam";
     public static final String USER_APP_KEY = "userAppKey";
     public static final String ENABLE_MUTLI_SESSION_RECORD = "enableMultiSessionRecord";
