221ad2272bd68f4e98962cdeac95ebfa7724163a
Rajendra Karki <rajendra@uxcam.com>
2024-12-19 10:34:59 +0545

    Update iOS SDK to 3.6.20 and Android SDK to 3.6.35

--- ios/Classes/FlutterUxcamPlugin.m
+++ ios/Classes/FlutterUxcamPlugin.m
@@ -23,7 +23,7 @@ static const NSString *FlutterExcludeScreens = @"excludeMentionedScreens";
     
     FlutterUxcamPlugin* instance = [[FlutterUxcamPlugin alloc] init];
     [registrar addMethodCallDelegate:instance channel:channel];
-    [UXCam pluginType:@"flutter" version:@"2.5.6"];
+    [UXCam pluginType:@"flutter" version:@"2.5.7"];
 }
 
 // The handler method - this is the entry point from the Dart code
