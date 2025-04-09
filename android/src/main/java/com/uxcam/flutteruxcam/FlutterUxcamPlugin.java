a2604ac5ccb62c3f7fe653f4efaac1e8bdab99c0
ayushshakya <ayush.shakya@uxcam.com>
2025-04-09 14:18:14 +0545

    add android folder in example

--- android/src/main/java/com/uxcam/flutteruxcam/FlutterUxcamPlugin.java
+++ android/src/main/java/com/uxcam/flutteruxcam/FlutterUxcamPlugin.java
@@ -8,7 +8,7 @@ import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.PluginRegistry.Registrar;
+//import io.flutter.plugin.common.PluginRegistry.Registrar;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
 import io.flutter.plugin.common.BinaryMessenger;
 import io.flutter.embedding.engine.plugins.activity.ActivityAware;
@@ -60,10 +60,10 @@ public class FlutterUxcamPlugin implements MethodCallHandler, FlutterPlugin, Act
      */
     private static Activity activity;
 
-    public static void registerWith(Registrar registrar) {
-        activity = registrar.activity();
-        register(registrar.messenger());
-    }
+    // public static void registerWith(Registrar registrar) {
+    //     activity = registrar.activity();
+    //     register(registrar.messenger());
+    // }
 
     @Override
     public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
