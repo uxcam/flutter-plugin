0f466fa6070ba4b26439aa897b3a25031d9a82ea
ayushshakya <ayush.shakya@uxcam.com>
2025-04-17 15:02:10 +0545

    add system gesture insets for certain samsung a5

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
 
     // Configuration
     FlutterUxConfig config = FlutterUxConfig(
-      userAppKey: 'UXCAM_API_KEY',
+      userAppKey: 'vwaxl2b5nx8i10z',
       // Important as this is handled by automatic screenTagging https://developer.uxcam.com/docs/tag-of-screens#control-automatic-tagging
       enableAutomaticScreenNameTagging: false,
     );
