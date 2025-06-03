979b218e53472a300c2b53eea67a3647598a996a
ayushshakya <ayush.shakya@uxcam.com>
2025-06-03 11:01:13 +0545

    correct visibility condition

--- android/src/main/java/com/uxcam/flutteruxcam/FlutterUxcamPlugin.java
+++ android/src/main/java/com/uxcam/flutteruxcam/FlutterUxcamPlugin.java
@@ -583,7 +583,7 @@ public class FlutterUxcamPlugin implements MethodCallHandler, FlutterPlugin, Act
                 Log.e(TAG, "Error parsing JSON", e);
             }
         }
-
+        
         ArrayList<Rect> result = new ArrayList<Rect>();
         for (Map.Entry<String, JSONArray> entry : widgetDataByKey.entrySet()) {
             String key = entry.getKey();
@@ -617,8 +617,7 @@ public class FlutterUxcamPlugin implements MethodCallHandler, FlutterPlugin, Act
 
                 }
             }
-            
-            if(keyVisibilityMap.get(key) != 2) {
+            if(keyVisibilityMap.get(key) < 2) {
                 output.left = output.left-5;
                 output.right = output.right+5;
                 output.top = output.top-5;
