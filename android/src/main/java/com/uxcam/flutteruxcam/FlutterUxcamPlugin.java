a1347823af9f9c4fdbdabb648e25542fb2e1e384
ayushshakya <ayush.shakya@uxcam.com>
2025-06-09 14:18:10 +0545

    add vertical bound offset

--- android/src/main/java/com/uxcam/flutteruxcam/FlutterUxcamPlugin.java
+++ android/src/main/java/com/uxcam/flutteruxcam/FlutterUxcamPlugin.java
@@ -627,8 +627,8 @@ public class FlutterUxcamPlugin implements MethodCallHandler, FlutterPlugin, Act
             if(keyVisibilityMap.get(key) < 2) {
                 output.left = output.left-15;
                 output.right = output.right+15;
-                output.top = output.top-15;
-                output.bottom = output.bottom+15;
+                output.top = output.top-25;
+                output.bottom = output.bottom+25;
                 result.add(output);
             }
         }
