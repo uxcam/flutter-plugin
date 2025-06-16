7760d76f266a13d56576f0bc8e88ad6afb419551
ayushshakya <ayush.shakya@uxcam.com>
2025-06-16 16:15:27 +0545

    remove unused occlusionChannel

--- lib/uxcam.dart
+++ lib/uxcam.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/services.dart';
 import 'package:flutter_uxcam/flutter_uxcam.dart';
 
 class UxCam {
@@ -7,10 +6,5 @@ class UxCam {
 
   UxCam() {
     dartTimeStamp = DateTime.now().millisecondsSinceEpoch;
-    const BasicMessageChannel<Object?> _occlusionRectsChannel =
-        BasicMessageChannel<Object?>(
-      'occlusion_rects_coordinates',
-      StandardMessageCodec(),
-    );
   }
 }
