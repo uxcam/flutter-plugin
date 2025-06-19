1656569d4078cded8830b76d080e415f3600fbce
Rajendra karki <rajendra@uxcam.com>
2025-06-19 11:23:48 +0545

    revert some changes.

--- lib/uxcam.dart
+++ lib/uxcam.dart
@@ -7,7 +7,8 @@ import 'package:flutter_uxcam/src/models/track_data.dart';
 
 class UxCam {
   static FlutterUxcamNavigatorObserver? navigationObserver;
-  final List<TrackData> _trackList = [];
+  List<TrackData> _trackList = [];
+  String _topRoute = "/";
 
   UxCam() {
     const BasicMessageChannel<Object?> uxCamMessageChannel =
@@ -21,17 +22,32 @@ class UxCam {
         (map["x"] as num).toDouble().toFlutter.toDouble(),
         (map["y"] as num).toDouble().toFlutter.toDouble(),
       );
+
       TrackData? _trackData;
       try {
-        trackData = _trackList.firstWhere((data) {
+        _trackData = _trackList.lastWhere((data) {
           return data.bound.contains(offset);
         });
-      } catch (e) {
-        print("No track data found for offset: $offset");
-      }
-      if (trackData != null) {
-        print("messagex: ${trackData.toString()}");
-        return jsonEncode(trackData.toJson());
+      } catch (e) {}
+      print("messagex:" + offset.toString());
+      print("messagex:" + _trackList.toString());
+      if (_trackData != null) {
+        if (_trackData.route != _topRoute) {
+          return "";
+        }
+
+        if (_trackData.route == "/") {
+          _trackData.route = "root";
+          if (_trackData.uiId != null) {
+            _trackData.uiId =
+                "root" + _trackData.uiId!.substring(1, _trackData.uiId!.length);
+          }
+        }
+        if (_trackData.uiId != null && _trackData.uiId!.startsWith("/")) {
+          _trackData.uiId =
+              _trackData.uiId!.substring(1, _trackData.uiId!.length);
+        }
+        return jsonEncode(_trackData.toJson());
       }
       return "";
     });
