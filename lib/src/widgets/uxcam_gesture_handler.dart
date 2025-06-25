e06777255eeed08770f798b6d412b1e00bf9b2c2
Rajendra karki <rajendra@uxcam.com>
2025-06-25 09:49:05 +0545

    fixed handler crash issue.

--- lib/src/widgets/uxcam_gesture_handler.dart
+++ lib/src/widgets/uxcam_gesture_handler.dart
@@ -31,6 +31,13 @@ class _UXCamGestureHandlerState extends State<UXCamGestureHandler> {
   Offset? _lastPointerDownLocation;
   TrackData? _lastTrackData;
 
+
+  @override
+  void initState() {
+    super.initState();
+    gestureHandler = GestureHandler();     
+  }
+
   @override
   Widget build(BuildContext context) {
     return Listener(
