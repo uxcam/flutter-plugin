7419aff0e8be450c9a7284e532ab92dd5c6c95b0
ayushshakya <ayush.shakya@uxcam.com>
2025-07-09 16:25:05 +0545

    add decoratedBox as a image

--- lib/flutter_uxcam.dart
+++ lib/flutter_uxcam.dart
@@ -8,7 +8,6 @@ export 'src/models/uxblur.dart';
 export 'src/models/uxoverlay.dart';
 export 'src/models/uxcam_config.dart';
 export './src/widgets/occlude_wrapper.dart';
-export './src/widgets/widget_capture.dart';
 export './src/widgets/uxcam_gesture_handler.dart';
 export './uxcam.dart';
 export 'src/helpers/uxcam_binding.dart';
