be349f30bdfe68e29d899fe0a9ffe55b6b21cbb8
ayushshakya <ayush.shakya@uxcam.com>
2025-07-14 08:17:48 +0545

    add prefix in element id

--- lib/src/models/track_data.dart
+++ lib/src/models/track_data.dart
@@ -55,7 +55,7 @@ class TrackData {
         jsonEncode(uiValue != null && uiValue!.isNotEmpty ? uiValue : "");
     return {
       'class': uiClass,
-      'id': jsonEncode(uiId),
+      'id': uiId,
       'value': value,
       'name': uiValue,
       'type': uiType,
