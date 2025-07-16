70db8c597252adc5560780bc4a49f0f68f19f2ff
ayushshakya <ayush.shakya@uxcam.com>
2025-07-16 13:26:18 +0545

    add animated opacity as a check for rendered elements

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -22,8 +22,8 @@ class MyApp extends StatelessWidget {
     // Configuration
     FlutterUxConfig config = FlutterUxConfig(
       //userAppKey: 'key',
-      userAppKey: 'gekzt6bh5299e09',
-      //userAppKey: 'vwaxl2b5nx8i10z',
+      //userAppKey: 'gekzt6bh5299e09',
+      userAppKey: 'vwaxl2b5nx8i10z',
       // Important as this is handled by automatic screenTagging https://developer.uxcam.com/docs/tag-of-screens#control-automatic-tagging
       enableAutomaticScreenNameTagging: true,
       enableIntegrationLogging: true,
