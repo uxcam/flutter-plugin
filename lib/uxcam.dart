673399f3a2d9233e8b9c4b2dc0b9a041e23e5282
ayushshakya <ayush.shakya@uxcam.com>
2025-07-29 19:36:28 +0545

    process track data in uxcam

--- lib/uxcam.dart
+++ lib/uxcam.dart
@@ -1,16 +1,72 @@
+import 'dart:convert';
+
 import 'package:flutter/services.dart';
 import 'package:flutter_uxcam/flutter_uxcam.dart';
+import 'package:flutter_uxcam/src/helpers/extensions.dart';
+import 'package:flutter_uxcam/src/models/track_data.dart';
 
 class UxCam {
   static FlutterUxcamNavigatorObserver? navigationObserver;
-  int? dartTimeStamp;
+  List<TrackData> _trackList = [];
+
+  String _topRoute = "/";
+  String get topRoute => _topRoute;
 
   UxCam() {
-    dartTimeStamp = DateTime.now().millisecondsSinceEpoch;
-    const BasicMessageChannel<Object?> _occlusionRectsChannel =
+    const BasicMessageChannel<Object?> uxCamMessageChannel =
         BasicMessageChannel<Object?>(
-      'occlusion_rects_coordinates',
+      'uxcam_message_channel',
       StandardMessageCodec(),
     );
+    uxCamMessageChannel.setMessageHandler((message) async {
+      print("flat tree:" + _trackList.toString());
+
+      final map = jsonDecode(message as String);
+      final offset = Offset(
+        (map["x"] as num).toDouble().toFlutter.toDouble(),
+        (map["y"] as num).toDouble().toFlutter.toDouble(),
+      );
+
+      TrackData? _trackData;
+      try {
+        _trackData = _trackList.firstWhere((data) {
+          return data.bound.contains(offset);
+        }).copy();
+      } catch (e) {}
+      if (_trackData != null) {
+        if (_trackData.route != _topRoute) {
+          return "";
+        }
+
+        if (_trackData.route == "/") {
+          _trackData.route = "root";
+          if (_trackData.uiId != null) {
+            _trackData.uiId = "root_${_trackData.uiClass!}_${_trackData.uiId!}";
+          }
+        } else {
+          _trackData.uiId =
+              "${_trackData.route.replaceAll(' ', '')}_${_trackData.uiClass!}_${_trackData.uiId!}";
+        }
+        print("messagey:" + _trackData.toString());
+        return jsonEncode(_trackData.toJson());
+      }
+      return "";
+    });
+  }
+
+  addWidgetDataForTracking(TrackData data) {
+    final id = data.uiId ?? "";
+    if (_trackList.indexWhere((item) => item.uiId == id) == -1) {
+      _trackList.add(data);
+    }
+  }
+
+  updateTopRoute(String route) {
+    _topRoute = route;
+    if (_topRoute == "") _topRoute = "/";
+  }
+
+  void removeTrackData() {
+    _trackList.clear();
   }
 }
