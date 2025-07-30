7846615a24831cda01f55e0856586e256d7bfbfd
ayushshakya <ayush.shakya@uxcam.com>
2025-07-30 17:09:57 +0545

    update android to 3.7.1 and ios to 3.6.26

--- ios/Classes/FlutterUxcamPlugin.m
+++ ios/Classes/FlutterUxcamPlugin.m
@@ -55,7 +55,7 @@ typedef void (^GestureEventCompletionBlock)(NSString* event);
         }];
     
     [registrar addMethodCallDelegate:instance channel:channel];
-    [UXCam pluginType:@"flutter" version:@"2.6.1"];
+    [UXCam pluginType:@"flutter" version:@"2.6.2"];
 }
 
 // The handler method - this is the entry point from the Dart code
