33b6df5380c48b2f6a5eec001caafe156980063e
ayushshakya <ayush.shakya@uxcam.com>
2025-11-24 17:59:35 +0545

    add log test

--- android/src/main/java/com/uxcam/flutteruxcam/FlutterUxcamPlugin.java
+++ android/src/main/java/com/uxcam/flutteruxcam/FlutterUxcamPlugin.java
@@ -129,6 +129,7 @@ public class FlutterUxcamPlugin implements MethodCallHandler, FlutterPlugin, Act
                 StandardMessageCodec.INSTANCE);
         uxcamMessageChannel.setMessageHandler((message, reply) -> {
             occlusionHandler.post(() -> {
+                Log.d("occ-test",message.toString())
                 processOcclusionRequest(message);
             });
         });
