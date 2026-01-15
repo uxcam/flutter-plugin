f3d25a05c683e256850a3538d390f223e58f0459
Rajendra karki <rajendra@uxcam.com>
2026-01-15 17:54:53 +0545

    made use of pause and resume pruely with android occlusion handler discarding deferframe logic.

--- lib/src/widgets/occlude_wrapper.dart
+++ lib/src/widgets/occlude_wrapper.dart
@@ -1,8 +1,6 @@
-import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_uxcam/src/widgets/occlude_wrapper_android.dart';
-import 'package:flutter_uxcam/src/widgets/occlude_wrapper_ios.dart';
 
 class OccludeWrapper extends StatelessWidget {
   final Widget child;
@@ -10,14 +8,8 @@ class OccludeWrapper extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    if (Platform.isIOS) {
-      return OccludeWrapperIos(
-        child: child,
-      );
-    } else {
-      return OccludeWrapperAndroid(
-        child: child,
-      );
-    }
+    return OccludeWrapperAndroid(
+      child: child,
+    );
   }
 }
