79859fc45caf40304113200984f32280323665cf
ayushshakya <ayush.shakya@uxcam.com>
2026-02-11 15:17:14 +0545

    dynamically inject web sdk configuration when startWithConfiguration is called

--- lib/src/flutter_uxcam_web.dart
+++ lib/src/flutter_uxcam_web.dart
@@ -25,7 +25,11 @@ class FlutterUxcamWeb {
   Future<dynamic> _handleMethodCall(MethodCall call) async {
     switch (call.method) {
       case 'startWithConfiguration':
-      //test event: remove later
+        final config = call.arguments['config'] as Map;
+        final appKey = config['userAppKey'] as String;
+        _injectWebSdk(appKey);
+        
+        //test event: remove later
         _sendEvent('flutter_plugin_connected', {'source': 'flutter_uxcam_web'});
         return true;
       case 'getPlatformVersion':
@@ -42,6 +46,30 @@ class FlutterUxcamWeb {
     }
   }
 
+  void _injectWebSdk(String appKey) {
+    _evalJs('''
+      window.uxc = {
+        __t: [],
+        __ak: "$appKey",
+        __o: {},
+        event: function(n, p) { this.__t.push(['event', n, p]); },
+        setUserIdentity: function(i) { this.__t.push(['setUserIdentity', i]); },
+        setUserProperty: function(k, v) { this.__t.push(['setUserProperty', k, v]); },
+        setUserProperties: function(p) { this.__t.push(['setUserProperties', p]); },
+        abort: function() { this.__t.push(['abort']); }
+      };
+      var head = document.getElementsByTagName('head')[0];
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = '//websdk-recording.uxcam.com/index.js';
+      script.async = true;
+      script.defer = true;
+      script.id = 'uxcam-web-sdk';
+      script.crossOrigin = 'anonymous';
+      head.appendChild(script);
+    '''.toJS);
+  }
+
   void _sendEvent(String name, Map<String, String> properties) {
     final uxc = _uxc;
     if (uxc == null) return;
@@ -52,3 +80,5 @@ class FlutterUxcamWeb {
 @JS('window.uxc.event')
 external void _uxcEvent(JSString name, JSAny? properties);
 
+@JS('eval')
+external void _evalJs(JSString code);
