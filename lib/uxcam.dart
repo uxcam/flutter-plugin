d6c730310ebf30000b1ff76e1f383f00f9da99fd
ayushshakya <ayush.shakya@uxcam.com>
2026-05-19 18:10:35 +0545

    initialize smart events from dartplugin class

--- lib/uxcam.dart
+++ lib/uxcam.dart
@@ -1,14 +1,32 @@
+import 'dart:async';
+
 import 'package:flutter/services.dart';
 import 'package:flutter_uxcam/src/smart_events/uxcam_smart_events.dart';
 
 class Uxcam {
-  
-    static void registerWith() {                                                                                                                                                               
-      const channel = MethodChannel('uxcam_smart_events');                                                                                                                                        
-      channel.setMethodCallHandler((call) async {
-        if (call.method == 'initSmartEvents') {                                                                                                                                                   
-          UXCamSmartEvents().initialize(enableGestureTracking: true);                                                                                                                             
-        }                                                                                                                                                                                         
-      });         
-    }  
+  static const MethodChannel _smartEventsChannel =
+      MethodChannel('uxcam_smart_events');
+
+  static void registerWith() {
+    _ensureHandlerSetup();
+  }
+
+  static void _ensureHandlerSetup() {
+    try {
+      _smartEventsChannel.setMethodCallHandler(_handleSmartEvents);
+    } catch (_) {
+      Timer(const Duration(milliseconds: 100), _ensureHandlerSetup);
+    }
+  }
+
+  static Future<dynamic> _handleSmartEvents(MethodCall call) async {
+    switch (call.method) {
+      case 'initSmartEvents':
+        UXCamSmartEvents().initialize(enableGestureTracking: true);
+        break;
+      default:
+        throw MissingPluginException(
+            'No handler for method ${call.method} on channel uxcam_smart_events');
+    }
+  }
 }
